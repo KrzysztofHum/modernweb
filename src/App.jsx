ed27a676daeabcd139fa2230bace8a01e80ccfb5
KrzysztofHum <k.humienny1996@protonmail.com>
2024-04-26 12:55:03 +0200

    add assets and careate button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import "./App.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <div className="pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+export default App;
