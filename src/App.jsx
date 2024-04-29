74fda08bcea6f9910ae380e1f2c6ed12d0a32973
KrzysztofHum <k.humienny1996@protonmail.com>
2024-04-29 08:32:02 +0200

    init hero section, add ai img

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <div className="pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
