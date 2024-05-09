e7081cde2d6002d9107366adae65409e229f14ce
KrzysztofHum <k.humienny1996@protonmail.com>
2024-05-09 14:46:01 +0200

    collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <div className="pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
